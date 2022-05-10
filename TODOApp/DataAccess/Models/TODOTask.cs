abfd686737c4bbfa1c243f3603e2fb981743c7df
nnicrow <nnicrow@gmail.com>
2022-05-10 22:40:48 +0300

    LeadTime from datetime to string

--- TODOApp/DataAccess/Models/TODOTask.cs
+++ TODOApp/DataAccess/Models/TODOTask.cs
@@ -6,7 +6,7 @@ public class TODOTask : BaseEntity
     public string? Description { get; set; }
     public DateTime CreatedOn { get; set; }
     
-    public DateTime? LeadTime { get; set; }
+    public string? LeadTime { get; set; }
     public bool IsComplete { get; set; } = false;
     public User Owmer { get; set; }
 }
